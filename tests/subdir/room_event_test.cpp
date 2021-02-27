e29bc70222aedc700cba55ad001d82affd086dad
Matthew Barnes <taking1n1@gmail.com>
2021-02-27 18:54:46 +0000

    Added room event tests

---
+++ tests/subdir/room_event_test.cpp
@@ -0,0 +1,100 @@
+#include <cstdio>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "game/game.hpp"
+#include "game/room.hpp"
+#include "gtest/gtest.h"
+
+namespace RoomEventTest
+{
+    class TestRoom : public Room
+    {
+    public:
+        int keyDownCount = 0;
+        int keyUpCount = 0;
+        int mouseMotionCount = 0;
+        int mouseButtonDownCount = 0;
+        int mouseButtonUpCount = 0;
+
+        void onKeyDown(const SDL_KeyboardEvent *) override { this->keyDownCount++; }
+
+        void onKeyUp(const SDL_KeyboardEvent *) override { this->keyUpCount++; }
+
+        void onMouseMotion(const SDL_MouseMotionEvent *) override { this->mouseMotionCount++; }
+
+        void onMouseButtonDown(const SDL_MouseButtonEvent *) override { this->mouseButtonDownCount++; }
+
+        void onMouseButtonUp(const SDL_MouseButtonEvent *) override { this->mouseButtonUpCount++; }
+    };
+
+    template <int event_type>
+    struct EventFixture : public testing::Test, testing::WithParamInterface<int>
+    {
+        Game *game;
+        TestRoom *room;
+
+        EventFixture()
+        {
+            SDL_Init(SDL_INIT_VIDEO);
+            IMG_Init(IMG_INIT_PNG);
+
+            SDL_Window *window;
+            SDL_Renderer *renderer;
+
+            SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+
+            this->game = new Game(renderer);
+            this->room = new TestRoom();
+            game->setCurrentRoom(this->room);
+        }
+
+        void SetUp() override
+        {
+            // Push events
+            SDL_Event event{.type = event_type};
+            auto noOfEvents = GetParam();
+
+            for (int i = 0; i < noOfEvents; i++)
+                SDL_PushEvent(&event);
+        }
+
+        void TearDown() override
+        {
+            this->room->keyDownCount = 0;
+            this->room->keyUpCount = 0;
+            this->room->mouseMotionCount = 0;
+            this->room->mouseButtonDownCount = 0;
+            this->room->mouseButtonUpCount = 0;
+        }
+
+        ~EventFixture()
+        {
+            delete this->game;
+            delete this->room;
+
+            SDL_Quit();
+        }
+    };
+}
+
+using namespace RoomEventTest;
+
+#define TEST_EVENT(EVENT_ENUM, EVENT_FIXTURE_NAME, EVENT_PROPERTY, TEST_NAME) \
+    struct EVENT_FIXTURE_NAME : public EventFixture<EVENT_ENUM>               \
+    {                                                                         \
+    };                                                                        \
+    TEST_P(EVENT_FIXTURE_NAME, TEST_NAME)                                     \
+    {                                                                         \
+        game->update();                                                       \
+        EXPECT_EQ(room->EVENT_PROPERTY, GetParam());                          \
+    }                                                                         \
+    INSTANTIATE_TEST_CASE_P(Default, EVENT_FIXTURE_NAME,                      \
+                            testing::Values(1, 2, 3, 4, 5));
+
+TEST_EVENT(SDL_KEYDOWN, KeyDownFixture, keyDownCount, room_should_handle_keydown_events)
+TEST_EVENT(SDL_KEYUP, KeyUpFixture, keyUpCount, room_should_handle_keyup_events)
+TEST_EVENT(SDL_MOUSEMOTION, MouseMotionFixture, mouseMotionCount, room_should_handle_mousemotion_events)
+TEST_EVENT(SDL_MOUSEBUTTONDOWN, MouseButtonDownFixture, mouseButtonDownCount, room_should_handle_mousebuttondown_events)
+TEST_EVENT(SDL_MOUSEBUTTONUP, MouseButtonUpFixture, mouseButtonUpCount, room_should_handle_mousebuttonup_events)
